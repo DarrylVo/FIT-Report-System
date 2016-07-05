ef5f368ff34ac9975884e4a8ee788756ba684e15
dvo <darrylvo@valleywater.org>
2016-07-05 18:28:15 +0000

    removed the unused d3 file, finally got the new report upload forum up and running with a new registration page for names

--- src/report.js
+++ src/report.js
@@ -2,10 +2,10 @@
 
 //global var for coordinates
 var coords = new Array(2);
-
+var globalForm;
 
 //sends a randomly located report to the server using ajax calls
-//TODO: add picture random report too
+//TODO: make this thing generate a full on report, possibly with cat pics
 function randomReport() {
    var la = Math.random() * (185 - 0) + 0;
    var lo = Math.random() * (185 - 0) + 0; 
@@ -18,6 +18,26 @@ function randomReport() {
          print.innerHTML = "Random report saved!";
       }  
    })
+}
+
+//updates the select drop down box with names from the mysql table 2
+//does this by doing ajax call/return from php
+function updateNames() {
+
+        var getnames = "getnames";
+	$.ajax({
+           type: "POST",
+           url: "src/report.php",
+           data:{ getnames : getnames }, 
+           success: function(data) {
+              var names_json = jQuery.parseJSON(data);
+              for(var i = 0; i < names_json.length; i++) {
+                 $("#cname").append($("<option></option>").attr("value", names_json[i].gps_name).text(names_json[i].gps_name));
+
+              }
+           }   
+       })
+
 }
 
 
@@ -38,8 +58,28 @@ $(function () {
     });
 });
 
-//another jquery call to do form validaiton
-//in progress callback function to get gps coords, then send all report data to server/php
+//jquery call to do form validation for the register name form
+$("#registerForm").validate({
+     submitHandler : registerName});
+
+//callback function for validation for the register name form
+
+function registerName(form) {
+
+   $(form).ajaxSubmit({
+        url : 'src/report.php',
+        type : 'POST',
+        success : function(data) {
+           console.log(data);
+           window.location = "http://scvwdflood.org/report.html";
+        }
+     });   
+
+
+}
+
+//another jquery call to do form validaiton for the main report form
+//callback function to get gps coords, then send all report data to server/php
 $("#commentForm").validate({
   rules: {
    pic: {
@@ -52,31 +92,36 @@ $("#commentForm").validate({
 });
 
 //callback function for validation
+//this function then uses the geolcation api to send gps coords along with the form to the server
 function sendForm(form) {
-
+var cname = $("#cname option:selected").text();
+alert(cname  );
+     globalForm = form;
      if (navigator.geolocation) 
         navigator.geolocation.getCurrentPosition(saveCoords);
      else { 
         alert("no gps support. update yo browser");
 
-      } 
-   $(form).ajaxSubmit({
+      }
+
+
+}
+
+//callback to geolocation api save coordinates
+//also submits it because the gps coordinates call is asychronous, so it must be done here.
+function saveCoords(position) {
+   coords[0] = position.coords.latitude;
+   coords[1] = position.coords.longitude;
+   $(globalForm).ajaxSubmit({
         url : 'src/report.php',
         type : 'POST',
         data : {coords : coords},
         processData: false,  // tell jQuery not to process the data
         contentType: false,  // tell jQuery not to set contentType
         success : function(data) {
-          location.reload(true);
+           console.log(data);
+           alert(data);
         }
      });   
-
-
-}
-
-//callback to save coordinates
-function saveCoords(position) {
-   coords[0] = position.coords.latitude;
-   coords[1] = position.coords.longitude;
 }
 
--- src/report.php
+++ src/report.php
@@ -1,7 +1,7 @@
 <?php
 //TODO: I REALLY NEED MYSQL SANITATION
 
-//how this works php script works- on ajax posts from either view.js or report.js it will do mysql queries
+//how this works php script works- on ajax posts from either view.js/report.js/register.js it will do mysql queries
 
 //creates mysqli connection object...   
 $mysqli = new mysqli("localhost", "root", "Applez255", "GPSCOORDS");
@@ -20,7 +20,7 @@ if($mysqli->connect_errno) {
 //use as the name of the uploaded photo
 //file extension is also saved in the mysql db so we can figure out the the the corresponding photo
 // i.e. for mysql record with a primary key of "1" and an extension of "png" the corresponding file is "1.png"
-//record to use as the picturename. makes the pics simple to track
+//makes the pics simple to track
 
 // DO i need further validation? *scratches head
 // oh yeah, mysql sanitation kek
@@ -28,6 +28,7 @@ else if(isset($_REQUEST['name'])){
    $coords = $_POST['coords'];
    $text = $_POST['text'];
    $name = $_POST['name'];
+   var_dump($name);
    $ext = end(explode(".",$_FILES['pic']['name']));
    $sql_q = "INSERT INTO GPSCOORDS_TB1 ".
        "(gps_lat, gps_long, gps_text, gps_ext, gps_name) ".
@@ -65,6 +66,22 @@ else if(isset($_REQUEST['name'])){
    fwrite($test, $fileContent);
    fclose($test);
         
+}
+
+// on this POST, registers the name and team id in the mysql database table 2!
+else if(isset($_REQUEST['namereg'])){
+   $teamid = $_POST['teamid'];
+   $name = $_POST['namereg'];
+   $sql_q = "INSERT INTO GPSCOORDS_TB2 ".
+            "(gps_teamid, gps_name)".
+            "VALUES".
+            "('$teamid', '$name')";
+   $result = mysqli_query($mysqli, $sql_q);
+   if(!$result) {
+      echo "error in registering name n stuff";
+   }
+   mysqli_free_result($result);
+
 }
 
 // on this POST, saves the random report
@@ -105,6 +122,25 @@ else if(isset($_REQUEST['getreports'])){
    $arr = array();
    $sql_q = 'SELECT gps_id, gps_lat, gps_long, gps_text, gps_ext, gps_name, gps_timestamp  
         FROM GPSCOORDS_TB1';
+   $retval = mysqli_query( $mysqli, $sql_q);
+   if(! $retval ) {
+      printf("getcoords error\n");
+      exit;
+   }
+   while($row = mysqli_fetch_array($retval, MYSQL_ASSOC))  
+      $arr[] = $row;
+
+   echo json_encode($arr);
+   mysqli_free_result($retval); 
+}
+
+//on this post, gets all the names out of the mysql db table 2!
+
+else if(isset($_REQUEST['getnames'])){
+
+   $arr = array();
+   $sql_q = 'SELECT gps_id, gps_name 
+        FROM GPSCOORDS_TB2';
    $retval = mysqli_query( $mysqli, $sql_q);
    if(! $retval ) {
       printf("getcoords error\n");
